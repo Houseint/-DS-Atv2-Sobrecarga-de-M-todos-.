0fbff907948350bc00786a3360eb525751399602
GustavoPitbull <83386216+Houseint@users.noreply.github.com>
2025-03-24 10:12:14 -0300

    Add files via upload

---
+++ Ex 2 Java/Conversor.java
@@ -0,0 +1,9 @@
+public class Conversor{
+    public float converter(float c){
+        return c*9/5 + 32;
+    }
+    public double converter(double f){
+        return (f-32)*5/9;
+    }
+    public Conversor(){}
+}
---
+++ Ex 2 Java/Main.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+
+public class Main{
+    public static void main(String[] args) {
+        Conversor conversor= new Conversor();
+        Scanner scanner=new Scanner(System.in);
+        int op;
+      do{  
+        System.out.println("Conversor de temperatura Celsius para Fahrenheit e vice versa.");
+        System.out.println("1- Celsius para Fahrenheit");
+        System.out.println("2- Fahrenheit para Celsius");
+    } while(!op=3);
+}
+}
